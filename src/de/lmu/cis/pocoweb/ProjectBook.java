f18c38a42fb75f78c04fd6de40818540a488b0e9
flo <flo@cis.lmu.de>
2018-01-16 14:27:10 +0100

    Implement addToProject

--- src/de/lmu/cis/pocoweb/ProjectBook.java
+++ src/de/lmu/cis/pocoweb/ProjectBook.java
@@ -1,9 +1,16 @@
 package de.lmu.cis.pocoweb;
 
 import com.google.gson.Gson;
+import java.util.List;
 import org.raml.jaxrs.example.model.Book;
+import org.raml.jaxrs.example.model.Project;
+import org.raml.jaxrs.example.model.ProjectEntry;
 
 class ProjectBook extends Book {
+  public ProjectBook() {
+    super();
+    this.description = "{}";
+  }
   public ProjectBook(Book s) {
     this.author = s.getAuthor();
     this.title = s.getTitle();
@@ -19,6 +26,7 @@ class ProjectBook extends Book {
       this.description = "{}";
     }
   }
+
   public void setOcrId(int id) {
     OcrData data = loadOcrData();
     data.ocrId = id;
@@ -39,6 +47,26 @@ class ProjectBook extends Book {
     setOcrData(data);
   }
   public String getOcrUser() { return loadOcrData().ocrUser; }
+
+  public void addToProject(Project project, String ocrEngine) {
+    // ocrId
+    List<ProjectEntry> list = project.getBooks();
+    if (list.isEmpty()) {
+      project.setProjectId(this.projectId);
+    }
+    // update book data
+    this.title = project.getTitle();
+    this.author = project.getAuthor();
+    this.language = project.getLanguage();
+    this.profilerUrl = project.getProfilerUrl();
+    this.year = project.getYear();
+    this.setOcrEngine(ocrEngine);
+    this.setOcrUser(project.getUser());
+    this.setOcrId(project.getProjectId());
+    // insert this book into project's list of books
+    list.add(new ProjectEntry().withOcrEngine(ocrEngine).withBook(this));
+    project.setBooks(list);
+  }
 
   private class OcrData {
     public String ocrUser, ocrEngine;
