46c05a3da2db2e22adff7545ce1be3ce079bb386
flo <flo@cis.lmu.de>
2018-01-16 10:59:54 +0100

    implement setters and getters for additonal ocr data

--- src/de/lmu/cis/pocoweb/ProjectBook.java
+++ src/de/lmu/cis/pocoweb/ProjectBook.java
@@ -1,5 +1,6 @@
 package de.lmu.cis.pocoweb;
 
+import com.google.gson.Gson;
 import org.raml.jaxrs.example.model.Book;
 
 class ProjectBook extends Book {
@@ -9,17 +10,46 @@ class ProjectBook extends Book {
     this.projectId = s.getProjectId();
     this.language = s.getLanguage();
     this.profilerUrl = s.getProfilerUrl();
-    this.description = s.getDescription();
     this.year = s.getYear();
     this.pages = s.getPages();
     this.isBook = s.getIsBook();
     this.pageIds = s.getPageIds();
+    this.description = s.getDescription();
+    if (description == null || description.isEmpty()) {
+      this.description = "{}";
+    }
   }
-  public void setOcrId(int id) {}
-  public int getOcrId() { return 0; }
+  public void setOcrId(int id) {
+    OcrData data = loadOcrData();
+    data.ocrId = id;
+    setOcrData(data);
+  }
+  public int getOcrId() { return loadOcrData().ocrId; }
 
-  public void setOcrEngine(String engine) {}
-  public String getOcrEngine() { return ""; }
-  public void setUser(String user) {}
-  public String getUser() { return ""; }
+  public void setOcrEngine(String engine) {
+    OcrData data = loadOcrData();
+    data.ocrEngine = engine;
+    setOcrData(data);
+  }
+
+  public String getOcrEngine() { return loadOcrData().ocrEngine; }
+
+  public void setOcrUser(String user) {
+    OcrData data = loadOcrData();
+    data.ocrUser = user;
+    setOcrData(data);
+  }
+
+  public String getOcrUser() { return loadOcrData().ocrUser; }
+
+  private class OcrData {
+    public String ocrUser, ocrEngine;
+    public int ocrId;
+  }
+  private OcrData loadOcrData() {
+    return new Gson().fromJson(description, OcrData.class);
+  }
+  private void setOcrData(OcrData data) {
+    description = new Gson().toJson(data);
+  }
 }
