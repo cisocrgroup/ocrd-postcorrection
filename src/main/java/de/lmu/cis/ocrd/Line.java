dc53c7880f129c10855564b24358814d418105c1
flo <flo@cis.lmu.de>
2018-01-23 16:40:58 +0100

    implement alignment between (normalized) text position and token

--- src/main/java/de/lmu/cis/ocrd/Line.java
+++ src/main/java/de/lmu/cis/ocrd/Line.java
@@ -9,6 +9,8 @@ import de.lmu.cis.pocoweb.Token;
 public class Line extends org.raml.jaxrs.example.model.Line {
   private final Client client;
   private final List<Token> tokens;
+  private String normalized;
+  private List<Token> tokenAlignements;
   public Line(Client client, org.raml.jaxrs.example.model.Line line)
       throws Exception {
     this.withProjectId(line.getProjectId())
@@ -23,25 +25,50 @@ public class Line extends org.raml.jaxrs.example.model.Line {
         .withIsPartiallyCorrected(line.getIsPartiallyCorrected())
         .withBox(line.getBox());
     this.client = client;
-    List<Token> tmp = client.getTokens(projectId, pageId, lineId);
-    if (tmp != null) {
-      this.tokens = tmp;
-    } else {
-      this.tokens = new ArrayList<Token>();
-    }
+    this.tokens = getTokens();
+    normalized = null;
+    tokenAlignements = null;
+    // List<Token> tmp = client.getTokens(projectId, pageId, lineId);
+    // if (tmp != null) {
+    //   this.tokens = tmp;
+    // } else {
+    //   this.tokens = new ArrayList<Token>();
+    // }
   }
 
   public String getNormalized() {
+    if (normalized == null || tokenAlignements == null) {
+      calculateNormalizedData();
+    }
+    return normalized;
+  }
+
+  public Token getTokenAt(int i) { return tokenAlignements.get(i); }
+
+  private void calculateNormalizedData() {
     StringBuilder builder = new StringBuilder();
     boolean first = true;
+    tokenAlignements = new ArrayList<Token>();
     for (Token t : tokens) {
       if (first) {
         first = false;
       } else {
         builder.append(' ');
+        tokenAlignements.add(null); // there is surely a better way
       }
       builder.append(t.getCor());
+      for (int i = 0; i < t.getCor().length(); i++) {
+        tokenAlignements.add(t);
+      }
     }
-    return builder.toString();
+    normalized = builder.toString();
+  }
+
+  private List<Token> getTokens() throws Exception {
+    List<Token> tokens = client.getTokens(projectId, pageId, lineId);
+    if (tokens == null) {
+      return new ArrayList<Token>();
+    }
+    return tokens;
   }
 }
