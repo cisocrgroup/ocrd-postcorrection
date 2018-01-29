d56b1a486b168d09e603c23d3e665ebcb47e2ca5
Tobias Englmeier <tobias.englmeier@hotmail.de>
2018-01-30 00:47:17 +0100

    start, find nodes for line alignment

--- src/main/java/de/lmu/cis/iba/Main.java
+++ src/main/java/de/lmu/cis/iba/Main.java
@@ -21,6 +21,44 @@ import de.lmu.cis.api.model.Project;
 import de.lmu.cis.api.model.Book;
 
 class Main {
+	
+  private static void check_sinks(Node node,Online_CDAWG_sym scdawg) {
+	  
+	  int sinkcount = 0; 
+	  
+	  	/// DIE KINDER DIE SINKS SIND SCHON MAL auslassen.. in der nächsten Runde.
+	    ArrayList nodes_to_all_sinks = new ArrayList();
+	    
+	    ArrayList sinks_hit = new ArrayList();
+	    
+	    Iterator it = node.children.entrySet().iterator();
+
+	      while (it.hasNext()) {
+	    	  
+	        Map.Entry pair = (Map.Entry)it.next();
+	        Node child = (Node)pair.getValue();
+	        
+	      
+	        	for (int j = 0; j < scdawg.sinks.size(); j++) {
+	        		if(scdawg.sinks.get(j)==child) {
+	        			sinkcount ++ ;
+	        			sinks_hit.add(scdawg.sinks.get(j));
+	        		}
+	        	}
+	         
+	    	  
+	      }
+	      
+	   
+	      for (int i=0;i<sinks_hit.size();i++) {
+	    	  System.out.println(sinks_hit.get(i));
+	      }
+	      
+      	System.out.println(sinkcount);
+
+	  
+  }
+	
   private static String patternsToString(String[] patterns) {
     String prefix = "[";
     String res = "";
@@ -97,98 +135,112 @@ class Main {
       scdawg.determineAlphabet(true);
       scdawg.build_cdawg();
       // scdawg.print_automaton("svgs/scdawg");
+      
+     Iterator it = scdawg.root.children.entrySet().iterator();
+     while (it.hasNext()) {
+    	
+    	 Map.Entry pair = (Map.Entry)it.next();
+         Node child = (Node)pair.getValue();
+    	 check_sinks(child,scdawg);
+    	 System.out.println(":::::::::::::::::::::::::::::::::");
+    	
+     }
+      
+//	 check_sinks(scdawg.root,scdawg);
 
       ArrayList test_result = new ArrayList();
+      
+      
 
       HashMap longest_quasi_nodes = new HashMap();
 
-      for (int j = 0; j < scdawg.sinks.size(); j++) {
-        for (int i = 0; i < scdawg.all_nodes.size(); i++) {
-          Node node = scdawg.all_nodes.get(i);
-
-          Iterator it = node.children.entrySet().iterator();
-
-          while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            Node child = (Node)pair.getValue();
-
-            if (scdawg.sinks.get(j) == child) {  // wenn rechtsübergang auf sink
-              test_result.add(scdawg.sinks.get(j).stringnr + " " +
-                              scdawg.get_node_label(node));
-
-              if (!longest_quasi_nodes.containsKey(
-                      scdawg.sinks.get(j).stringnr)) {
-                longest_quasi_nodes.put(scdawg.sinks.get(j).stringnr,
-                                        scdawg.get_node_label(node));
-
-              } else {
-                if (scdawg.get_node_length(node) >
-                    longest_quasi_nodes.get(scdawg.sinks.get(j).stringnr)
-                        .toString()
-                        .length()) {
-                  longest_quasi_nodes.put(scdawg.sinks.get(j).stringnr,
-                                          scdawg.get_node_label(node));
-                }
-              }
-
-              Iterator it2 = node.children_left.entrySet().iterator();
-
-              while (it2.hasNext()) {
-                Map.Entry pair_left = (Map.Entry)it2.next();
-                Node child_left = (Node)pair_left.getValue();
-
-                if (scdawg.sinks.get(j) ==
-                    child_left) {  // wenn linksübergang auf sink
-                }
-              }
-            }
-          }
-        }
-      }
-
-      ArrayList<ArrayList> all_aligned_lines = new ArrayList<ArrayList>();
-
-      Iterator it = longest_quasi_nodes.entrySet().iterator();
-
-      while (it.hasNext()) {
-        Map.Entry pair = (Map.Entry)it.next();
-
-        int key1 = (int)pair.getKey();
-        String value1 = (String)pair.getValue();
-        System.out.println(key1 + " " + value1);
-
-        if (value1 == null) continue;
-
-        ArrayList<Integer> aligned_lines = new ArrayList();
-        aligned_lines.add(key1);
-
-        Iterator it2 = longest_quasi_nodes.entrySet().iterator();
-
-        //			  while (it2.hasNext()) {
-        //			      Map.Entry pair2 = (Map.Entry)it2.next();
-        //
-        //			      int key2 = (int) pair2.getKey();
-        //				  String value2 =  (String)
-        //pair2.getValue();
-        //
-        //			      if (key1!=key2&&value1.equals(value2)) {
-        //
-        //			    	  aligned_lines.add(key2);
-        //
-        //			    	  longest_quasi_nodes.put(key2, null);
-        //
-        //			      }
-        //			  }
-        //
-        //			  all_aligned_lines.add(aligned_lines);
-      }
-
-      for (ArrayList aligned_lines : all_aligned_lines) {
-        for (int i = 0; i < aligned_lines.size(); i++) {
-          System.out.println(aligned_lines.get(i));
-        }
-        System.out.println("-------------------");
-      }
+//      for (int j = 0; j < scdawg.sinks.size(); j++) {
+//        for (int i = 0; i < scdawg.all_nodes.size(); i++) {
+//          Node node = scdawg.all_nodes.get(i);
+//
+//          Iterator it = node.children.entrySet().iterator();
+//
+//          while (it.hasNext()) {
+//            Map.Entry pair = (Map.Entry)it.next();
+//            Node child = (Node)pair.getValue();
+//
+//            if (scdawg.sinks.get(j) == child) {  // wenn rechtsübergang auf sink
+//              test_result.add(scdawg.sinks.get(j).stringnr + " " +
+//                              scdawg.get_node_label(node));
+//
+//              if (!longest_quasi_nodes.containsKey(
+//                      scdawg.sinks.get(j).stringnr)) {
+//                longest_quasi_nodes.put(scdawg.sinks.get(j).stringnr,
+//                                        scdawg.get_node_label(node));
+//
+//              } else {
+//                if (scdawg.get_node_length(node) >
+//                    longest_quasi_nodes.get(scdawg.sinks.get(j).stringnr)
+//                        .toString()
+//                        .length()) {
+//                  longest_quasi_nodes.put(scdawg.sinks.get(j).stringnr,
+//                                          scdawg.get_node_label(node));
+//                }
+//              }
+//
+//              Iterator it2 = node.children_left.entrySet().iterator();
+//
+//              while (it2.hasNext()) {
+//                Map.Entry pair_left = (Map.Entry)it2.next();
+//                Node child_left = (Node)pair_left.getValue();
+//
+//                if (scdawg.sinks.get(j) ==
+//                    child_left) {  // wenn linksübergang auf sink
+//                }
+//              }
+//            }
+//          }
+//        }
+//      }
+//
+//      ArrayList<ArrayList> all_aligned_lines = new ArrayList<ArrayList>();
+//
+//      Iterator it = longest_quasi_nodes.entrySet().iterator();
+//
+//      while (it.hasNext()) {
+//        Map.Entry pair = (Map.Entry)it.next();
+//
+//        int key1 = (int)pair.getKey();
+//        String value1 = (String)pair.getValue();
+//        System.out.println(key1 + " " + value1);
+//
+//        if (value1 == null) continue;
+//
+//        ArrayList<Integer> aligned_lines = new ArrayList();
+//        aligned_lines.add(key1);
+//
+//        Iterator it2 = longest_quasi_nodes.entrySet().iterator();
+//
+//        //			  while (it2.hasNext()) {
+//        //			      Map.Entry pair2 = (Map.Entry)it2.next();
+//        //
+//        //			      int key2 = (int) pair2.getKey();
+//        //				  String value2 =  (String)
+//        //pair2.getValue();
+//        //
+//        //			      if (key1!=key2&&value1.equals(value2)) {
+//        //
+//        //			    	  aligned_lines.add(key2);
+//        //
+//        //			    	  longest_quasi_nodes.put(key2, null);
+//        //
+//        //			      }
+//        //			  }
+//        //
+//        //			  all_aligned_lines.add(aligned_lines);
+//      }
+//
+//      for (ArrayList aligned_lines : all_aligned_lines) {
+//        for (int i = 0; i < aligned_lines.size(); i++) {
+//          System.out.println(aligned_lines.get(i));
+//        }
+//        System.out.println("-------------------");
+//      }
     
 
       client.deleteProject(project);
