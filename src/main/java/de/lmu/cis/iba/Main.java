0fba8a92f7a4f7eb403eccb3f12512cabb29c6a0
flo <flo@cis.lmu.de>
2018-02-07 17:24:22 +0100

    Bla

--- src/main/java/de/lmu/cis/iba/Main.java
+++ src/main/java/de/lmu/cis/iba/Main.java
@@ -27,6 +27,7 @@ import de.lmu.cis.api.model.Project;
 import de.lmu.cis.api.model.Book;
 
 class Main {
+  private static final int N = 3;
 
   static ArrayList<Node> sinks = new ArrayList();
 
@@ -63,70 +64,65 @@ class Main {
     return sortedHashMap;
   }
 
-  private static void find_n_transitions_to_sinks(Node node,Online_CDAWG_sym scdawg) {
-	    
-	    Iterator it = node.children.entrySet().iterator();
+  private static HashSet<Integer>
+  find_n_transitions_to_sinks(Node node, Online_CDAWG_sym scdawg,
+                              HashSet<Integer> acc) {
 
-	      while (it.hasNext()) {
-	    	  
-	        Map.Entry pair = (Map.Entry)it.next();
-	        Node child = (Node)pair.getValue();
-	       		      
-	        	for (int j = 0; j < scdawg.sinks.size(); j++) {
-	        		if(scdawg.sinks.get(j)==child) {
-	        			if(!sinks.contains(scdawg.sinks.get(j))) {
-	        				for (int k = 0 ; k < scdawg.sinks.get(j).stringnumbers.size();k++) {
-	        				sinks.add(scdawg.sinks.get(j));
-	        				}
-	        			}
-	        		}
-	        	}
-	         
-	    	  
-	      }
-	      
-	  		     
-	      Iterator it2 = node.children_left.entrySet().iterator();
+    Iterator it = node.children.entrySet().iterator();
+    HashSet<Integer> result = new HashSet<Integer>();
+    while (it.hasNext()) {
+      Map.Entry pair = (Map.Entry)it.next();
+      Node child = (Node)pair.getValue();
+      for (int j = 0; j < scdawg.sinks.size(); j++) {
+        if (scdawg.sinks.get(j) == child) {
+          if (!sinks.contains(scdawg.sinks.get(j))) {
+            for (int k = 0; k < scdawg.sinks.get(j).stringnumbers.size(); k++) {
+              acc.add(scdawg.sinks.get(j).stringnumbers.get(k));
+              // sinks.add(scdawg.sinks.get(j));
+            }
+          }
+        }
+      }
+    }
 
-	      while (it2.hasNext()) {
-	    	  
-	        Map.Entry pair = (Map.Entry)it2.next();
-	        Node child = (Node)pair.getValue();
-	      
-	        	for (int j = 0; j < scdawg.sinks.size(); j++) {
-	        		if(scdawg.sinks.get(j)==child) {
-	        			if(!sinks.contains(scdawg.sinks.get(j))) {
-	        				for (int k = 0 ; k < scdawg.sinks.get(j).stringnumbers.size();k++) {
-	        				sinks.add(scdawg.sinks.get(j));
-	        				}
-	        			}
-	        		}
-	        	}
-	         
-	    	  
-	      }
-	    			   			      
-	      
-	      
-	      // REC AUFRUF der Funktion mit den Kindern
-	      Iterator it3 = node.children.entrySet().iterator();
+    Iterator it2 = node.children_left.entrySet().iterator();
 
-	      while (it3.hasNext()) {
+    while (it2.hasNext()) {
 
-	        Map.Entry pair = (Map.Entry)it3.next();
-	        Node child = (Node)pair.getValue();
-	        find_n_transitions_to_sinks(child,scdawg);
-	      }
-	      
-	      Iterator it4 = node.children_left.entrySet().iterator();
+      Map.Entry pair = (Map.Entry)it2.next();
+      Node child = (Node)pair.getValue();
 
-	      while (it4.hasNext()) {
-	        Map.Entry pair = (Map.Entry)it4.next();
-	        Node child = (Node)pair.getValue();
-	        find_n_transitions_to_sinks(child,scdawg);
-	      }
-	  
-}
+      for (int j = 0; j < scdawg.sinks.size(); j++) {
+        if (scdawg.sinks.get(j) == child) {
+          if (!sinks.contains(scdawg.sinks.get(j))) {
+            for (int k = 0; k < scdawg.sinks.get(j).stringnumbers.size(); k++) {
+              acc.add(scdawg.sinks.get(j).stringnumbers.get(k));
+              // sinks.add(scdawg.sinks.get(j));
+            }
+          }
+        }
+      }
+    }
+
+    // REC AUFRUF der Funktion mit den Kindern
+    Iterator it3 = node.children.entrySet().iterator();
+
+    while (it3.hasNext()) {
+
+      Map.Entry pair = (Map.Entry)it3.next();
+      Node child = (Node)pair.getValue();
+      find_n_transitions_to_sinks(child, scdawg, acc);
+    }
+
+    Iterator it4 = node.children_left.entrySet().iterator();
+
+    while (it4.hasNext()) {
+      Map.Entry pair = (Map.Entry)it4.next();
+      Node child = (Node)pair.getValue();
+      find_n_transitions_to_sinks(child, scdawg, acc);
+    }
+    return acc;
+  }
 
   private static void count_nodes(Node node, Online_CDAWG_sym scdawg,
                                   HashMap<Node, Integer> result) {
@@ -180,6 +176,10 @@ class Main {
     }
   }
 
+  private static class pair {
+    public Node node;
+    public HashSet<Integer> ids;
+  }
   public static void main(String[] args) {
     try (Client client = Client.login(Config.getInstance().getPocowebURL(),
                                       Config.getInstance().getPocowebUser(),
@@ -219,7 +219,7 @@ class Main {
       Document doc = new Document(project, client);
 
       ArrayList<String> stringset = new ArrayList<String>();
-      ArrayList<String> ids = new ArrayList<String>();
+      ArrayList<String> strids = new ArrayList<String>();
       doc.eachLine(new Document.Visitor() {
         public void visit(Document.LineTriple t) throws Exception {
           System.out.println(String.format("[%9s,%1d,%2d] %s", t.ocrEngine,
@@ -230,13 +230,13 @@ class Main {
                 "[token %2d] %s", token.getTokenId(), token.getCor()));
           }
           stringset.add("#" + t.line.getNormalized() + "$");
-          ids.add(String.format("[%d,%d,%s]", t.pageSeq, t.line.getLineId(),
-                                t.ocrEngine));
+          strids.add(String.format("[%d,%d,%s]", t.pageSeq, t.line.getLineId(),
+                                   t.ocrEngine));
         }
       });
 
       for (int i = 0; i < stringset.size(); i++) {
-        System.out.println(stringset.get(i) + ": " + ids.get(i));
+        System.out.println(stringset.get(i) + ": " + strids.get(i));
       }
 
       Online_CDAWG_sym scdawg = new Online_CDAWG_sym(stringset, false);
@@ -249,76 +249,64 @@ class Main {
       count_nodes(scdawg.root, scdawg, nodes_count);
 
       HashMap count_nodes_sorted = sort_by_values_desc(nodes_count);
-      ArrayList<ArrayList> nodes_sink_set = new ArrayList();
+      ArrayList<pair> nodes_sink_set = new ArrayList<pair>();
 
+      count_nodes_sorted.put(scdawg.root, null);
       Iterator it3 = count_nodes_sorted.entrySet().iterator();
 
+      HashSet<Integer> usedIDs = new HashSet<Integer>();
+    main_loop:
       while (it3.hasNext()) {
         Map.Entry pair = (Map.Entry)it3.next();
 
         Node n = (Node)pair.getKey();
 
+        HashSet<Integer> ids =
+            find_n_transitions_to_sinks(n, scdawg, new HashSet<Integer>());
 
-        sinks = new ArrayList();
-
-        find_n_transitions_to_sinks(n, scdawg);
-
-        if (sinks.size() != 3)
+        if (ids.size() != N) {
           continue;
-        
-//      System.out.println(scdawg.get_node_label((Node)pair.getKey())+" "+pair.getValue());
-//      System.out.println(scdawg.get_node_label((Node)pair.getKey())+" "+sinks.size());
-
-        ArrayList<Node> sink_set = new ArrayList();
-
-        for (int i = 0; i < sinks.size(); i++) {
-          sink_set.add(sinks.get(i));
         }
-
-        
-        
-        if (nodes_sink_set.size() == 0)
-          nodes_sink_set.add(new ArrayList() {
-            {
-              add(n);
-              add(sink_set);
-            }
-          });
-        else {
-          boolean node_found = false;
-          for (int i = 0; i < nodes_sink_set.size(); i++) {
-
-            ArrayList pair2 = nodes_sink_set.get(i);
-
-            for (int j = 0; j < sink_set.size(); j++) {
-              ArrayList<Node> known_sinks = (ArrayList)nodes_sink_set.get(i).get(1);
-              for (int k = 0; k < known_sinks.size(); k++)
-
-                if (known_sinks.get(k).equals(sink_set.get(j))) {
-                	//System.out.println(scdawg.get_node_label(n)+" :: " + scdawg.get_node_label(sink_set.get(j))+" FOUND!!!");
-                  node_found = true;
-                }
-            }
+        for (Integer id : ids) {
+          if (usedIDs.contains(id)) {
+            continue main_loop;
           }
-          if (!node_found)
-            nodes_sink_set.add(new ArrayList() {
-              {
-                add(n);
-                add(sink_set);
-                
-              }
-            });
+        }
+        for (Integer id : ids) {
+          usedIDs.add(id);
+        }
+        pair p = new pair();
+        p.ids = ids;
+        p.node = n;
+        nodes_sink_set.add(p);
+      }
+      // handle final nodes (special case if all ocrs are identical)
+      for (Node sink : scdawg.sinks) {
+        System.out.println("size: " + sink.stringnumbers.size());
+        if (sink.stringnumbers.size() == N) {
+          // it is impossilbe (?) that this node was used before
+          System.out.println("got sink with " + N + " sinks");
+          System.out.println(sink.stringnumbers);
+          pair p = new pair();
+          p.ids = new HashSet<Integer>();
+          for (Integer id : sink.stringnumbers) {
+            p.ids.add(id);
+          }
+          p.node = scdawg.root;
+          nodes_sink_set.add(p);
         }
       }
 
-//       ArrayList<String> xyz = new ArrayList<String>(stringset.size());
+      //       ArrayList<String> xyz = new ArrayList<String>(stringset.size());
       String[] xyz = new String[stringset.size()];
-      for (int i = 0; i < nodes_sink_set.size(); i++) {
-        ArrayList pair = nodes_sink_set.get(i);
-        System.out.println(scdawg.get_node_label((Node)pair.get(0)));
-        for (int j = 0; j < ((ArrayList)pair.get(1)).size(); j++) {
-          int idx = ((Node)((ArrayList)pair.get(1)).get(j)).stringnr;
-          System.out.println(" - " + stringset.get(idx) + ": " + ids.get(idx));
+      for (pair p : nodes_sink_set) {
+        System.out.println(scdawg.get_node_label(p.node));
+        System.out.println(p.ids);
+        for (Integer id : p.ids) {
+          int idx = id;
+
+          System.out.println("- " + stringset.get(idx) + ": " +
+                             strids.get(idx));
           xyz[idx] = stringset.get(idx);
         }
         System.out.println();
