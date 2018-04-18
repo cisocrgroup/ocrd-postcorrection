79ecd077f40af952a722c75e937271222db94014
flo <flo@cis.lmu.de>
2018-04-18 16:29:04 +0200

    Finish implementation of page xml parser

--- src/main/java/de/lmu/cis/ocrd/parsers/PageXMLParser.java
+++ src/main/java/de/lmu/cis/ocrd/parsers/PageXMLParser.java
@@ -1,11 +1,25 @@
 package de.lmu.cis.ocrd.parsers;
 
+import javax.xml.xpath.XPathException;
 import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 public class PageXMLParser extends AbstractXPathParser {
+	private static final XPathExpression linesXPath;
+
+	private static final XPathExpression wordsXPath;
+
+	static {
+		try {
+			linesXPath = XPathFactory.newInstance().newXPath().compile("//TextLine");
+			wordsXPath = XPathFactory.newInstance().newXPath().compile("./Word/TextEquiv/Unicode");
+		} catch (XPathException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	protected PageXMLParser(Document xml, int pageID) {
 		super(xml, pageID);
@@ -14,20 +28,20 @@ public class PageXMLParser extends AbstractXPathParser {
 
 	@Override
 	protected XPathExpression getLinesXPath() {
-		// TODO Auto-generated method stub
-		return null;
+		return linesXPath;
 	}
 
 	@Override
 	protected XPathExpression getWordsXPath() {
-		// TODO Auto-generated method stub
-		return null;
+		return wordsXPath;
 	}
 
 	@Override
 	Word parseWord(Node word) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (word.getFirstChild() == null || word.getFirstChild().getNodeType() != Node.TEXT_NODE) {
+			throw new Exception("invalid page XML word node: missing unicode content");
+		}
+		return new Word(word.getFirstChild().getNodeValue(), false).fillConfidences(0);
 	}
 
 }
