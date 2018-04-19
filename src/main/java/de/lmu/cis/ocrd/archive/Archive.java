803adecba4293faf0ca3b30788354540cd47feaf
flo <flo@cis.lmu.de>
2018-04-19 16:54:06 +0200

    Use AutoCloseable interface for Archive

--- src/main/java/de/lmu/cis/ocrd/archive/Archive.java
+++ src/main/java/de/lmu/cis/ocrd/archive/Archive.java
@@ -1,12 +1,11 @@
 package de.lmu.cis.ocrd.archive;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 
-public interface Archive extends Closeable, Iterable<Entry> {
-		public Path getName();
+public interface Archive extends AutoCloseable, Iterable<Entry> {
+	public Path getName();
 
-		public InputStream open(Entry entry) throws IOException;
+	public InputStream open(Entry entry) throws IOException;
 }
