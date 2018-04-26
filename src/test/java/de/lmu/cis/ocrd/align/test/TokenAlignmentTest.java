e6e909faa3362b30e8eab257cc4241fbbfefbdfd
flo <flo@cis.lmu.de>
2018-04-26 10:08:28 +0200

    Add more tests for multiple token alignment

--- src/test/java/de/lmu/cis/ocrd/align/test/TokenAlignmentTest.java
+++ src/test/java/de/lmu/cis/ocrd/align/test/TokenAlignmentTest.java
@@ -9,12 +9,34 @@ import de.lmu.cis.ocrd.align.TokenAlignment;
 
 public class TokenAlignmentTest {
 	@Test
-	public void test() {
+	public void test2alignments() {
 		final String a = "abc def";
 		final String b = "a b c def";
 		TokenAlignment tokens = new TokenAlignment(a).add(b);
 		assertThat(tokens.size(), is(2));
 		assertThat(tokens.get(0).toString(), is("abc|a,b,c"));
 		assertThat(tokens.get(1).toString(), is("def|def"));
+	}
+
+	@Test
+	public void test3alignments() {
+		final String a = "abcd ef gh";
+		final String b = "ab cd efgh";
+		final String c = "ab cd ef gh";
+		TokenAlignment tokens = new TokenAlignment(a).add(b).add(c);
+		assertThat(tokens.size(), is(3));
+		assertThat(tokens.get(0).toString(), is("abcd|ab,cd|ab,cd"));
+		assertThat(tokens.get(1).toString(), is("ef|efgh|ef"));
+		assertThat(tokens.get(2).toString(), is("gh|efgh|gh"));
+	}
+
+	@Test
+	public void testAlignToSelf() {
+		final String a = "abc def ghi";
+		TokenAlignment tokens = new TokenAlignment(a).add(a);
+		assertThat(tokens.size(), is(3));
+		assertThat(tokens.get(0).toString(), is("abc|abc"));
+		assertThat(tokens.get(1).toString(), is("def|def"));
+		assertThat(tokens.get(2).toString(), is("ghi|ghi"));
 	}
 }
