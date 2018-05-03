cdef97434b962d9eb1c635b68828ac2aad99b327
flo <flo@cis.lmu.de>
2018-05-03 10:15:04 +0200

    Reproduce locale-based error

---
+++ src/test/java/de/lmu/cis/ocrd/ml/test/ARFFWriterDELocaleTest.java
@@ -0,0 +1,19 @@
+package de.lmu.cis.ocrd.ml.test;
+
+import java.util.Locale;
+
+public class ARFFWriterDELocaleTest extends ARFFWriterTest {
+    @Override
+    public void init() throws Exception {
+        Locale old = Locale.getDefault();
+        try {
+            Locale.setDefault(new Locale("de", "DE"));
+            super.init();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            Locale.setDefault(old);
+        }
+    }
+}
+
