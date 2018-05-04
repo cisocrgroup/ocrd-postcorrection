2bf233d5fae78e4e92f15a5818a57d77c1e6a378
flo <flo@cis.lmu.de>
2018-05-04 15:31:43 +0200

    Implement tests for ocorpus llocs and gt file archives

--- src/main/java/de/lmu/cis/ocrd/FileTypes.java
+++ src/main/java/de/lmu/cis/ocrd/FileTypes.java
@@ -15,143 +15,158 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class FileTypes {
-		public enum ArchiveType {
-				ZIP, DIR //, FILE, METS
-		}
-		// order of types is used to breaks ties:
-		// PAGEXML is preferred, then ALTOXML ...
-		public enum OCRType {
-				PAGEXML, ALTOXML, ABBYY, OCROPUS, HOCR
-		}
-		public static class Type {
-				private final ArchiveType archiveType;
-				private final OCRType ocrType;
-				public Type(ArchiveType archiveType, OCRType ocrType) {
-						this.archiveType = archiveType;
-						this.ocrType = ocrType;
-				}
-				public ArchiveType getArchiveType() {
-						return this.archiveType;
-				}
-				public OCRType getOCRType() {
-						return this.ocrType;
-				}
-		}
+    public enum ArchiveType {
+        ZIP, DIR //, FILE, METS
+    }
 
-		public static Type guess(String path) throws Exception {
-				if (path.endsWith(".zip") || path.endsWith(".ZIP")) {
-						Logger.debug("{} is a zip archive", path);
-						return guess(ArchiveType.ZIP, path);
-				}
-				if (Files.isDirectory(Paths.get(path))) {
-						Logger.debug("{} is a directory", path);
-						return guess(ArchiveType.DIR, path);
-				}
-				throw new Exception("cannot automatically determine type of file: " + path);
-		}
+    // order of types is used to breaks ties:
+    // PAGEXML is preferred, then ALTOXML ...
+    public enum OCRType {
+        PAGEXML, ALTOXML, ABBYY, OCROPUS_LLOCS, OCROPUS_GT, OCROPUS, HOCR
+    }
 
-		public static Type guess(ArchiveType archiveType, String path) throws Exception {
-				try(final Archive ar = newArchive(archiveType, path)) {
-						final TreeMap<OCRType, Integer> counts = new TreeMap<OCRType, Integer>(); // use ordered map to break ties
-						final Map<OCRType, XMLFileType> types = newOCRTypeMap();
-						for (Entry entry : ar) {
-								updateCounts(counts, types, entry);
-						}
-					return guess(archiveType, counts);
-				}
-		}
+    public static class Type {
+        private final ArchiveType archiveType;
+        private final OCRType ocrType;
 
-		public static Document openDocument(String path) throws Exception {
-		    return openDocument(guess(path), path);
+        public Type(ArchiveType archiveType, OCRType ocrType) {
+            this.archiveType = archiveType;
+            this.ocrType = ocrType;
         }
 
-		public static Document openDocument(Type type, String path) throws Exception {
-		    try (final Archive ar = newArchive(type.getArchiveType(), path)) {
-		        return newArchiveParser(type, ar).parse();
+        public ArchiveType getArchiveType() {
+            return this.archiveType;
+        }
+
+        public OCRType getOCRType() {
+            return this.ocrType;
+        }
+    }
+
+    public static Type guess(String path) throws Exception {
+        if (path.endsWith(".zip") || path.endsWith(".ZIP")) {
+            Logger.debug("{} is a zip archive", path);
+            return guess(ArchiveType.ZIP, path);
+        }
+        if (Files.isDirectory(Paths.get(path))) {
+            Logger.debug("{} is a directory", path);
+            return guess(ArchiveType.DIR, path);
+        }
+        throw new Exception("cannot automatically determine type of file: " + path);
+    }
+
+    public static Type guess(ArchiveType archiveType, String path) throws Exception {
+        try (final Archive ar = newArchive(archiveType, path)) {
+            final TreeMap<OCRType, Integer> counts = new TreeMap<>(); // use ordered map to break ties
+            final Map<OCRType, XMLFileType> types = newOCRTypeMap();
+            for (Entry entry : ar) {
+                updateCounts(counts, types, entry);
+            }
+            return guess(archiveType, counts);
+        }
+    }
+
+    public static Document openDocument(String path) throws Exception {
+        return openDocument(guess(path), path);
+    }
+
+    public static Document openDocument(Type type, String path) throws Exception {
+        try (final Archive ar = newArchive(type.getArchiveType(), path)) {
+            return newArchiveParser(type, ar).parse();
+        }
+    }
+
+    private static Type guess(ArchiveType archiveType, TreeMap<OCRType, Integer> counts) throws Exception {
+        int max = -1;
+        OCRType argMax = OCRType.PAGEXML;
+        for (Map.Entry<OCRType, Integer> entry : counts.entrySet()) {
+            Logger.debug("{}: {}", entry.getKey(), entry.getValue());
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                argMax = entry.getKey();
             }
         }
-
-		private static Type guess(ArchiveType archiveType, TreeMap<OCRType, Integer> counts) throws Exception {
-			int max = -1;
-			OCRType argMax = OCRType.PAGEXML;
-			for (Map.Entry<OCRType, Integer> entry : counts.entrySet()) {
-				Logger.debug("{}: {}", entry.getKey(), entry.getValue());
-				if (entry.getValue() > max) {
-					max = entry.getValue();
-					argMax = entry.getKey();
-				}
-			}
-			if (max <= 0) {
-				throw new Exception("cannot determine type of archive: no usable files");
-			}
-			return new Type(archiveType, argMax);
-		}
-
-		private static void updateCounts(Map<OCRType, Integer> counts, Map<OCRType, XMLFileType> types, Entry entry) {
-				for (Map.Entry<OCRType, XMLFileType> type : types.entrySet()) {
-						if (type.getValue().check(entry.getName().toString())) {
-								Logger.debug("found file {} of type {}", entry.getName(), type.getKey());
-								if (!counts.containsKey(type.getKey())) {
-										counts.put(type.getKey(), 0);
-								}
-								final int n = counts.get(type.getKey());
-								counts.put(type.getKey(), n+1);
-						}
-				}
-		}
-		private static Map<OCRType, XMLFileType> newOCRTypeMap() {
-				Map<OCRType, XMLFileType> map = new HashMap<OCRType, XMLFileType>();
-				for (OCRType t : OCRType.values()) {
-						map.put(t, newXMLFileType(t));
-				}
-				return map;
-		}
-
-		public static Parser newArchiveParser(Type type, Archive ar) throws Exception {
-		    if (type.getOCRType() == OCRType.OCROPUS) {
-		        return new OcropusArchiveParser(ar);
-            }
-            final OCRType ocrType = type.getOCRType();
-            return new ArchiveParser(newXMLParserFactory(ocrType), newXMLFileType(ocrType), ar);
+        if (max <= 0) {
+            throw new Exception("cannot determine type of archive: no usable files");
         }
+        return new Type(archiveType, argMax);
+    }
 
-		public static XMLParserFactory newXMLParserFactory(OCRType ocrType) throws Exception {
-            switch(ocrType) {
-                case PAGEXML:
-                    return new PageXMLParserFactory();
-                case ALTOXML:
-                    return new ALTOXMLParserFactory();
-                case ABBYY:
-                    return new ABBYYXMLParserFactory();
-                case HOCR:
-                    return new HOCRParserFactory();
+    private static void updateCounts(Map<OCRType, Integer> counts, Map<OCRType, XMLFileType> types, Entry entry) {
+        for (Map.Entry<OCRType, XMLFileType> type : types.entrySet()) {
+            if (type.getValue().check(entry.getName().toString())) {
+                Logger.debug("found file {} of type {}", entry.getName(), type.getKey());
+                if (!counts.containsKey(type.getKey())) {
+                    counts.put(type.getKey(), 0);
+                }
+                final int n = counts.get(type.getKey());
+                counts.put(type.getKey(), n + 1);
             }
-            throw new Exception("invalid OCR type: " + ocrType);
         }
+    }
 
-		public static XMLFileType newXMLFileType(OCRType ocrType) {
-				switch(ocrType) {
-				case PAGEXML:
-						return new PageXMLFileType();
-				case ALTOXML:
-						return new ALTOXMLFileType();
-				case ABBYY:
-						return new ABBYYXMLFileType();
-				case OCROPUS:
-						return new OcropusFileType();
-				case HOCR:
-						return new HOCRFileType();
-				}
-				throw new RuntimeException("non-exhaustive switch");
-		}
+    private static Map<OCRType, XMLFileType> newOCRTypeMap() {
+        Map<OCRType, XMLFileType> map = new HashMap<>();
+        for (OCRType t : OCRType.values()) {
+            map.put(t, newXMLFileType(t));
+        }
+        return map;
+    }
 
-		public static Archive newArchive(ArchiveType archiveType, String path) throws IOException {
-				switch (archiveType) {
-				case ZIP:
-						return new ZipArchive(path);
-				case DIR:
-						return new DirectoryArchive(path);
-				}
-			throw new RuntimeException("non-exhaustive switch");
-		}
+    public static Parser newArchiveParser(Type type, Archive ar) throws Exception {
+        switch (type.getOCRType()) {
+            case OCROPUS_LLOCS:
+                return new OcropusArchiveLlocsParser(ar);
+            case OCROPUS:
+                return new OcropusArchiveParser(ar);
+            case OCROPUS_GT:
+                return new OcropusArchiveGTParser(ar);
+        }
+        final OCRType ocrType = type.getOCRType();
+        return new ArchiveParser(newXMLParserFactory(ocrType), newXMLFileType(ocrType), ar);
+    }
+
+    public static XMLParserFactory newXMLParserFactory(OCRType ocrType) throws Exception {
+        switch (ocrType) {
+            case PAGEXML:
+                return new PageXMLParserFactory();
+            case ALTOXML:
+                return new ALTOXMLParserFactory();
+            case ABBYY:
+                return new ABBYYXMLParserFactory();
+            case HOCR:
+                return new HOCRParserFactory();
+        }
+        throw new Exception("invalid OCR type: " + ocrType);
+    }
+
+    public static XMLFileType newXMLFileType(OCRType ocrType) {
+        switch (ocrType) {
+            case PAGEXML:
+                return new PageXMLFileType();
+            case ALTOXML:
+                return new ALTOXMLFileType();
+            case ABBYY:
+                return new ABBYYXMLFileType();
+            case OCROPUS_LLOCS:
+                return new OcropusLlocsFileType();
+            case OCROPUS_GT:
+                return new OcropusGTFileType();
+            case OCROPUS:
+                return new OcropusFileType();
+            case HOCR:
+                return new HOCRFileType();
+        }
+        throw new RuntimeException("non-exhaustive switch");
+    }
+
+    public static Archive newArchive(ArchiveType archiveType, String path) throws IOException {
+        switch (archiveType) {
+            case ZIP:
+                return new ZipArchive(path);
+            case DIR:
+                return new DirectoryArchive(path);
+        }
+        throw new RuntimeException("non-exhaustive switch");
+    }
 }
--- src/test/java/de/lmu/cis/ocrd/test/GuessFileTypeTest.java
+++ src/test/java/de/lmu/cis/ocrd/test/GuessFileTypeTest.java
@@ -1,13 +1,12 @@
 package de.lmu.cis.ocrd.test;
+import de.lmu.cis.ocrd.FileTypes;
+import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Test;
-
-import de.lmu.cis.ocrd.FileTypes;
-
 public class GuessFileTypeTest {
-	
+
 	@Test
 	public void testABBYY() throws Exception {
 		final String resource = "src/test/resources/1841-DieGrenzboten-abbyy.zip";
@@ -29,8 +28,25 @@ public class GuessFileTypeTest {
 		final String resource = "src/test/resources/1841-DieGrenzboten-ocropus.zip";
 		FileTypes.Type type = FileTypes.guess(resource);
 		assertThat(type.getArchiveType(), is(FileTypes.ArchiveType.ZIP));
+		assertThat(type.getOCRType(), is(FileTypes.OCRType.OCROPUS_LLOCS));
+	}
+
+	@Test
+	public void testOcropusNoLlocs() throws Exception {
+		final String resource = "src/test/resources/1841-DieGrenzboten-ocropus-no-llocs.zip";
+		FileTypes.Type type = FileTypes.guess(resource);
+		assertThat(type.getArchiveType(), is(FileTypes.ArchiveType.ZIP));
 		assertThat(type.getOCRType(), is(FileTypes.OCRType.OCROPUS));
 	}
+
+	@Test
+	public void testOcropusGT() throws Exception {
+		final String resource = "src/test/resources/1841-DieGrenzboten-ocropus-gt.zip";
+		FileTypes.Type type = FileTypes.guess(resource);
+		assertThat(type.getArchiveType(), is(FileTypes.ArchiveType.ZIP));
+		assertThat(type.getOCRType(), is(FileTypes.OCRType.OCROPUS_GT));
+	}
+
 
 	@Test
 	public void testPageXML() throws Exception {
