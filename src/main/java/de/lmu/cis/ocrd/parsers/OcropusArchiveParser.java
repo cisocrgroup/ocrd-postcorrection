b69386c682c78fb5c695c8ebdb74b6456c17ae5a
flo <flo@cis.lmu.de>
2018-05-04 10:28:08 +0200

    Use lambda for sorting

--- src/main/java/de/lmu/cis/ocrd/parsers/OcropusArchiveParser.java
+++ src/main/java/de/lmu/cis/ocrd/parsers/OcropusArchiveParser.java
@@ -1,20 +1,17 @@
 package de.lmu.cis.ocrd.parsers;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-
-import org.apache.commons.io.IOUtils;
-
 import de.lmu.cis.ocrd.SimpleDocument;
 import de.lmu.cis.ocrd.SimpleLine;
 import de.lmu.cis.ocrd.archive.Archive;
 import de.lmu.cis.ocrd.archive.Entry;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class OcropusArchiveParser implements Parser {
 	private static boolean isOcropusLine(String name) {
@@ -28,17 +25,14 @@ public class OcropusArchiveParser implements Parser {
 	}
 
 	private static void sort(List<Entry> lines) {
-		Collections.sort(lines, new Comparator<Entry>() {
-			@Override
-			public int compare(Entry a, Entry b) {
-				int c = a.getName().getParent().getFileName().toString()
-						.compareTo(b.getName().getParent().getFileName().toString());
-				if (c != 0) {
-					return c;
-				}
-				return a.getName().toString().compareTo(b.getName().toString());
-
+		lines.sort((a, b) -> {
+			int c = a.getName().getParent().getFileName().toString()
+					.compareTo(b.getName().getParent().getFileName().toString());
+			if (c != 0) {
+				return c;
 			}
+			return a.getName().toString().compareTo(b.getName().toString());
+
 		});
 	}
 
@@ -51,7 +45,7 @@ public class OcropusArchiveParser implements Parser {
 	@Override
 	public SimpleDocument parse() throws Exception {
 		// gather valid `.txt` files
-		ArrayList<Entry> entries = new ArrayList<Entry>();
+		ArrayList<Entry> entries = new ArrayList<>();
 		for (Entry entry : this.ar) {
 			if (!isOcropusLine(entry.getName().toString())) {
 				continue;
@@ -61,7 +55,7 @@ public class OcropusArchiveParser implements Parser {
 		sort(entries);
 		// append lines to document
 		SimpleDocument doc = new SimpleDocument().withPath(ar.getName().toString());
-		HashMap<Integer, Integer> lineIDs = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> lineIDs = new HashMap<>();
 		for (Entry entry : entries) {
 			int pageno = Integer.parseInt(entry.getName().getParent().getFileName().toString());
 			if (!lineIDs.containsKey(pageno)) {
