f273eb6045ff14f3bc2ed021ba610884faaba50f
flo <flo@cis.lmu.de>
2018-05-07 13:27:29 +0200

    Add debug messages for testBug1

--- src/main/java/de/lmu/cis/ocrd/align/Graph.java
+++ src/main/java/de/lmu/cis/ocrd/align/Graph.java
@@ -2,6 +2,7 @@ package de.lmu.cis.ocrd.align;
 
 import de.lmu.cis.iba.Pairwise_LCS_Alignment;
 import de.lmu.cis.iba.Pairwise_LCS_Alignment.AlignmentPair;
+import org.pmw.tinylog.Logger;
 
 import java.util.ArrayList;
 
@@ -61,7 +62,7 @@ public class Graph {
 			final Node curn = new Node(curp.label);
 			final Gap g1 = makeGap(prevp.epos1, curp.spos1, s1, curn);
 			final Gap g2 = makeGap(prevp.epos2, curp.spos2, s2, curn);
-            // Logger.info("s1: '{}'\nINFO: s2: '{}'\nINFO: pl: '{}'\nINFO: g1: '{}'\nINFO: g2: '{}'\nINFO: cl: '{}'", s1, s2, prevn.getLabel(), g1.getLabel(), g2.getLabel(), curn.getLabel());
+            Logger.info("s1: '{}'\nINFO: s2: '{}'\nINFO: pl: '{}'\nINFO: g1: '{}'\nINFO: g2: '{}'\nINFO: cl: '{}'", s1, s2, prevn.getLabel(), g1.getLabel(), g2.getLabel(), curn.getLabel());
 			prevn.add(g1);
 			prevn.add(g2);
 			prevp = curp;
@@ -70,18 +71,20 @@ public class Graph {
 	}
 
 	private AlignmentPair handleOverlap(AlignmentPair previous, AlignmentPair current) {
-		// Logger.info("previous.epos1: {}, previous.epos2: {}, previous.label: '{}'", previous.epos1, previous.epos2, previous.label);
-		// Logger.info("current.spos1: {}, current.spos2: {}, current.label: '{}'", current.spos1, current.spos2, current.label);
-		// Logger.info("s1: '{}'\nINFO: s2: '{}'", s1, s2);
-		// Logger.info("sub1: '{}'\nINFO: sub2: '{}'", s1.substring(previous.epos1), s2.substring(previous.epos2));
-		if (previous.epos1 > current.spos1) {
-			String label = current.label.substring(previous.epos1 - current.spos1);
-			return new AlignmentPair(label, current.epos1, current.epos2);
-		}
-		if (previous.epos2 > current.spos2) {
-			String label = current.label.substring(previous.epos2 - current.spos2);
-			return new AlignmentPair(label, current.epos1, current.epos2);
-		}
+		Logger.info("previous.epos1: {}, previous.epos2: {}, previous.label: '{}'", previous.epos1, previous.epos2, previous.label);
+		Logger.info("current.spos1: {}, current.spos2: {}, current.label: '{}'", current.spos1, current.spos2, current.label);
+		Logger.info("s1: '{}'\nINFO: s2: '{}'", s1, s2);
+		Logger.info("sub1: '{}'\nINFO: sub2: '{}'", s1.substring(previous.epos1), s2.substring(previous.epos2));
+	    Logger.info("previous: {}", previous);
+        Logger.info("current:  {}", current);
+		// if (previous.epos1 > current.spos1) {
+		// 	String label = current.label.substring(previous.epos1 - current.spos1);
+		// 	return new AlignmentPair(label, current.epos1, current.epos2);
+		// }
+		// if (previous.epos2 > current.spos2) {
+		// 	String label = current.label.substring(previous.epos2 - current.spos2);
+		// 	return new AlignmentPair(label, current.epos1, current.epos2);
+		// }
 		return current;
 	}
 
