422959420dc3ec3bcf4f5f482bdebbbb98118270
flo <flo@cis.lmu.de>
2018-05-07 16:20:36 +0200

    Implement factory method for TokenLengthFeature

--- src/main/java/de/lmu/cis/ocrd/ml/features/TokenLengthFeature.java
+++ src/main/java/de/lmu/cis/ocrd/ml/features/TokenLengthFeature.java
@@ -1,9 +1,17 @@
 package de.lmu.cis.ocrd.ml.features;
 
+import com.google.gson.JsonObject;
 import de.lmu.cis.ocrd.ml.Token;
 
 public class TokenLengthFeature extends NamedBooleanFeature{
     private final int min, max;
+
+    public static TokenLengthFeature fromJSON(JsonObject configuration) {
+        return new TokenLengthFeature(
+                JSONUtil.mustGet(configuration, "min").getAsInt(),
+                JSONUtil.mustGet(configuration, "max").getAsInt(),
+                JSONUtil.mustGetValidName(configuration));
+    }
 
     public TokenLengthFeature(int min, int max, String name) {
         super(name);
