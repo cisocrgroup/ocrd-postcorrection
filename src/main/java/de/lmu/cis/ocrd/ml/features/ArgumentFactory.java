6da3f7b9ec8266a322eea0137295df8b922301f4
flo <flo@cis.lmu.de>
2018-05-08 10:37:56 +0200

    Change getProfiler to getProfile

--- src/main/java/de/lmu/cis/ocrd/ml/features/ArgumentFactory.java
+++ src/main/java/de/lmu/cis/ocrd/ml/features/ArgumentFactory.java
@@ -1,10 +1,10 @@
 package de.lmu.cis.ocrd.ml.features;
 
 import de.lmu.cis.ocrd.ml.FreqMap;
-import de.lmu.cis.ocrd.profile.Profiler;
+import de.lmu.cis.ocrd.profile.Profile;
 
 public interface ArgumentFactory {
     FreqMap<String> getOCRUnigrams();
     FreqMap<String> getCharacterTrigrams();
-    Profiler getProfiler();
+    Profile getProfile();
 }
