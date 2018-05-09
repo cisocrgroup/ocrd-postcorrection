908c727162274d59d43fcfb112ca917f6bc91196
flo <flo@cis.lmu.de>
2018-05-09 11:34:50 +0200

    Implement two simple text and string parsers for testing

---
+++ src/main/java/de/lmu/cis/ocrd/parsers/TextParser.java
@@ -0,0 +1,34 @@
+package de.lmu.cis.ocrd.parsers;
+
+import com.google.common.io.LineReader;
+import de.lmu.cis.ocrd.SimpleDocument;
+import de.lmu.cis.ocrd.SimpleLine;
+
+import java.io.Reader;
+
+public class TextParser implements Parser {
+    private final Reader reader;
+    private final int pageID;
+
+    public TextParser(int pageID, Reader reader) {
+        this.reader = reader;
+        this.pageID = pageID;
+    }
+
+    @Override
+    public SimpleDocument parse() throws Exception {
+        final SimpleDocument document = new SimpleDocument();
+        final LineReader lr = new LineReader(reader);
+        String line;
+        int lineID = 0;
+        while ((line = lr.readLine()) != null) {
+            // skip empty lines
+            if (line.trim().isEmpty()) {
+                continue;
+            }
+            final SimpleLine l = SimpleLine.normalized(line, 0).withLineID(lineID).withPageID(pageID);
+            document.add(pageID, l);
+        }
+        return document;
+    }
+}
