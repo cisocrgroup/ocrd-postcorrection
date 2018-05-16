7394418f8dd02508bcfd3d132f037094e820c1cf
flo <flo@cis.lmu.de>
2018-05-16 13:36:47 +0200

    Add (failing) test for unique page, line and token IDs

---
+++ src/test/java/de/lmu/cis/ocrd/train/test/TokenizerTest.java
@@ -0,0 +1,55 @@
+package de.lmu.cis.ocrd.train.test;
+
+import de.lmu.cis.ocrd.train.Environment;
+import de.lmu.cis.ocrd.train.Tokenizer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TokenizerTest {
+    private static final String base = "src/test/resources";
+    private static final String name = "training-test";
+    private Environment environment;
+    private Tokenizer tokenizer;
+
+    @Before
+    public void init() throws IOException {
+        environment = new Environment(base, name)
+                .withCopyTrainingFiles(false)
+                .withGT(base + "/1841-DieGrenzboten-gt-small.zip")
+                .withMasterOCR(base + "/1841-DieGrenzboten-abbyy-small.zip")
+                .addOtherOCR(base + "/1841-DieGrenzboten-tesseract-small.zip");
+        tokenizer = new Tokenizer(environment);
+    }
+
+    @Test
+    public void testUniqueIDs() throws Exception {
+        final HashMap<Integer, HashMap<Integer, HashSet<Integer>>> pageOfLineOfToken = new HashMap<>();
+        tokenizer.eachToken((token)->{
+            final int pageID = token.getMasterOCR().getLine().getPageId();
+            final int lineID = token.getMasterOCR().getLine().getLineId();
+            final int tokenID = token.getID();
+            if (!pageOfLineOfToken.containsKey(pageID)) {
+                pageOfLineOfToken.put(pageID, new HashMap<>());
+            }
+            if (!pageOfLineOfToken.get(pageID).containsKey(lineID)) {
+                pageOfLineOfToken.get(pageID).put(lineID, new HashSet<>());
+            }
+            assertThat(pageOfLineOfToken.get(pageID).get(lineID).contains(tokenID), is(false));
+            pageOfLineOfToken.get(pageID).get(lineID).add(tokenID);
+            assertThat(pageOfLineOfToken.get(pageID).get(lineID).contains(tokenID), is(true));
+        });
+    }
+
+    @After
+    public void deInit() throws IOException {
+        environment.remove();
+    }
+}
