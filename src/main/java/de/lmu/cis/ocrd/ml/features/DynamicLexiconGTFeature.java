e65dec00d6693bfa991a39d146acb2d3a50cd8ff
flo <flo@cis.lmu.de>
2018-05-18 11:21:44 +0200

    Ignore gt for small tokens and return false for those

--- src/main/java/de/lmu/cis/ocrd/ml/features/DynamicLexiconGTFeature.java
+++ src/main/java/de/lmu/cis/ocrd/ml/features/DynamicLexiconGTFeature.java
@@ -6,6 +6,7 @@ import de.lmu.cis.ocrd.ml.Token;
 // equals its ground-truth. This feature should be used to simply add GT-data to
 // the training and evaluation steps.
 public class DynamicLexiconGTFeature extends NamedBooleanFeature {
+    private final int SHORT = 3; // setting of the profiler
     public DynamicLexiconGTFeature() {
         super("DynamicLexiconGT");
     }
@@ -15,8 +16,13 @@ public class DynamicLexiconGTFeature extends NamedBooleanFeature {
         return handlesOnlyMasterOCR(i, n);
     }
 
+    // Returns true if a OCR token should be taken into the dynamic lexicon for the next
+    // profiler run. Any OCR token that is correct should be taken into the profiler lexicon.
+    // If tokens are smaller than 4 letters, the profiler ignores them, so small tokens
+    // should never be put into the dynamic lexicon.
     @Override
     boolean doCalculate(Token token, int i, int n) {
-        return token.isCorrectToken();
+        final String str = token.getMasterOCR().toString();
+        return str.codePointCount(0, str.length()) > SHORT && token.isCorrectToken();
     }
 }
