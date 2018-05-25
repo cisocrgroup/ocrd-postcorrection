1431d58383b84dc95f24b7785b1ae81c6aa85663
flo <flo@cis.lmu.de>
2018-05-25 15:40:33 +0200

    Add test for ProfilerHighestVoteWeightFeature

--- src/test/java/de/lmu/cis/ocrd/ml/test/DefaultFeatureFactoryTest.java
+++ src/test/java/de/lmu/cis/ocrd/ml/test/DefaultFeatureFactoryTest.java
@@ -52,4 +52,13 @@ public class DefaultFeatureFactoryTest {
 		assertThat(feature.get().getName(), is("MaxCharNGrams"));
 		assertThat(feature.get() instanceof MaxCharNGramsFeature, is(true));
 	}
+
+	@Test
+	public void testHighestProfilerVoteWeightFeature() throws Exception {
+		final String json = "{\"type\":\"de.lmu.cis.ocrd.ml.features.ProfilerHighestVoteWeightFeature\",\"name\":\"ProfilerHighestVoteWeight\"}";
+		final Optional<Feature> feature = makeFeature(json);
+		assertThat(feature.isPresent(), is(true));
+		assertThat(feature.get().getName(), is("ProfilerHighestVoteWeight"));
+		assertThat(feature.get() instanceof ProfilerHighestVoteWeightFeature, is(true));
+	}
 }
