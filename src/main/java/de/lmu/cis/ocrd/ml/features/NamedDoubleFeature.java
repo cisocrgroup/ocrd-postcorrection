679c2ebd1342e49b1c6d74104ee3e86bfed668f7
flo <flo@cis.lmu.de>
2018-06-07 12:11:17 +0200

    Check for NaN

--- src/main/java/de/lmu/cis/ocrd/ml/features/NamedDoubleFeature.java
+++ src/main/java/de/lmu/cis/ocrd/ml/features/NamedDoubleFeature.java
@@ -9,7 +9,11 @@ public abstract class NamedDoubleFeature extends NamedFeature {
 
     @Override
     public final Object calculate(Token token, int i, int n) {
-        return this.doCalculate(token, i, n);
+		final Double res = doCalculate(token, i, n);
+//        if (res.isNaN() || res.isInfinite()) {
+//        	throw new RuntimeException("token: " + token.toJSON() + " (" + i + ") (" + n + ") isNAN() or isInfinite()");
+//		}
+		return res;
     }
 
     protected abstract double doCalculate(Token token, int i, int n);
