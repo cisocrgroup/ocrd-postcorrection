a34083280854c4fa2900e284ccf74743f0313c25
flo <flo@cis.lmu.de>
2018-10-10 17:14:18 +0200

    Clean up

--- src/test/java/de/lmu/cis/ocrd/train/test/EnvironmentTest.java
+++ src/test/java/de/lmu/cis/ocrd/train/test/EnvironmentTest.java
@@ -13,8 +13,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class EnvironmentTest extends TestBase {
 
