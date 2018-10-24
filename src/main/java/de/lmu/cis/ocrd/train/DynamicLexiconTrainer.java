b98accf0b12b36f9b197acbb78347939287901e6
flo <flo@cis.lmu.de>
2018-10-24 16:37:39 +0200

    Indentation

--- src/main/java/de/lmu/cis/ocrd/train/DynamicLexiconTrainer.java
+++ src/main/java/de/lmu/cis/ocrd/train/DynamicLexiconTrainer.java
@@ -51,12 +51,16 @@ public class DynamicLexiconTrainer {
 	}
 
 	private void prepare(int n) throws Exception {
-		final Path trainPath = environment.fullPath(environment.getDynamicLexiconTrainingFile(n));
-		final Path testPath = environment.fullPath(environment.getDynamicLexiconTestFile(n));
+		final Path trainPath = environment
+				.fullPath(environment.getDynamicLexiconTrainingFile(n));
+		final Path testPath = environment
+				.fullPath(environment.getDynamicLexiconTestFile(n));
 		final FeatureSet fs = newFeatureSet();
 		final List<Token> tokens = new ArrayList<>();
-		try (final TrainingAndTestARFFWriter w = new TrainingAndTestARFFWriter(fs, trainPath, testPath)) {
-			w.withDebug(environment.openConfiguration().getDynamicLexiconTrainig().isDebugTrainingTokens());
+		try (final TrainingAndTestARFFWriter w = new TrainingAndTestARFFWriter(
+				fs, trainPath, testPath)) {
+			w.withDebug(environment.openConfiguration()
+					.getDynamicLexiconTrainig().isDebugTrainingTokens());
 			w.withRelationName("DynamicLexiconExpansion_" + n);
 			w.writeHeader(n);
 			newTrainSetSplitter().eachToken((Token token, boolean isTrain) -> {
@@ -70,8 +74,10 @@ public class DynamicLexiconTrainer {
 				}
 			});
 		}
-		final Path tokensPath = environment.fullPath(environment.getDynamicLexiconTestTokensFile(n));
-		try (final ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(tokensPath.toFile()))) {
+		final Path tokensPath = environment
+				.fullPath(environment.getDynamicLexiconTestTokensFile(n));
+		try (final ObjectOutputStream out = new ObjectOutputStream(
+				new FileOutputStream(tokensPath.toFile()))) {
 			out.writeObject(tokens);
 			out.flush();
 		}
@@ -79,28 +85,34 @@ public class DynamicLexiconTrainer {
 
 	private FeatureSet newFeatureSet() throws Exception {
 		return FeatureFactory.getDefault().withArgumentFactory(environment)
-				.createFeatureSet(environment.openConfiguration().getDynamicLexiconTrainig().getFeatures())
+				.createFeatureSet(environment.openConfiguration()
+						.getDynamicLexiconTrainig().getFeatures())
 				.add(new DynamicLexiconGTFeature());
 	}
 
 	private TrainSetSplitter newTrainSetSplitter() throws IOException {
-		final int n = environment.openConfiguration().getDynamicLexiconTrainig().getTestEvaluationFraction();
+		final int n = environment.openConfiguration().getDynamicLexiconTrainig()
+				.getTestEvaluationFraction();
 		final Tokenizer tokenizer = new Tokenizer(environment);
 		return new TrainSetSplitter(tokenizer, n);
 	}
 
 	private void train(int n) throws Exception {
 		Logger.info("training for {} OCR(s)", n);
-		final Path trainingFile = environment.fullPath(environment.getDynamicLexiconTrainingFile(n));
-		final ConverterUtils.DataSource dataSource = new ConverterUtils.DataSource(trainingFile.toString());
+		final Path trainingFile = environment
+				.fullPath(environment.getDynamicLexiconTrainingFile(n));
+		final ConverterUtils.DataSource dataSource = new ConverterUtils.DataSource(
+				trainingFile.toString());
 		final Instances train = dataSource.getDataSet();
 		train.setClassIndex(train.numAttributes() - 1);
 		final Instances structure = dataSource.getStructure();
 		structure.setClassIndex(structure.numAttributes() - 1);
 		final AbstractClassifier logistic = new SimpleLogistic();
 		logistic.buildClassifier(train);
-		final Path modelFile = environment.fullPath(environment.getDynamicLexiconModel(n));
-		try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(modelFile.toFile()))) {
+		final Path modelFile = environment
+				.fullPath(environment.getDynamicLexiconModel(n));
+		try (ObjectOutputStream out = new ObjectOutputStream(
+				new FileOutputStream(modelFile.toFile()))) {
 			out.writeObject(logistic);
 			out.flush();
 		}
@@ -108,8 +120,10 @@ public class DynamicLexiconTrainer {
 
 	private void evaluate(int n) throws Exception {
 		Logger.info("evaluating {} OCR(s)", n);
-		final Path testFile = environment.fullPath(environment.getDynamicLexiconTestFile(n));
-		final ConverterUtils.DataSource dataSource = new ConverterUtils.DataSource(testFile.toString());
+		final Path testFile = environment
+				.fullPath(environment.getDynamicLexiconTestFile(n));
+		final ConverterUtils.DataSource dataSource = new ConverterUtils.DataSource(
+				testFile.toString());
 		final Instances test = dataSource.getDataSet();
 		test.setClassIndex(test.numAttributes() - 1);
 		final Instances structure = dataSource.getStructure();
@@ -117,10 +131,12 @@ public class DynamicLexiconTrainer {
 		final AbstractClassifier logistic = openClassifier(n);
 		final Evaluation evaluation = new Evaluation(structure);
 		evaluation.evaluateModel(logistic, test);
-		final Path evaluationFile = environment.fullPath(environment.getDynamicLexiconEvaluationFile(n));
-		try (final Writer w = new BufferedWriter(new FileWriter(evaluationFile.toString()))) {
-			final String summary = evaluation.toSummaryString(String.format("\n Results (%d)\n=============\n", n),
-					true);
+		final Path evaluationFile = environment
+				.fullPath(environment.getDynamicLexiconEvaluationFile(n));
+		try (final Writer w = new BufferedWriter(
+				new FileWriter(evaluationFile.toString()))) {
+			final String summary = evaluation.toSummaryString(
+					String.format("\n Results (%d)\n=============\n", n), true);
 			w.write(summary);
 			w.write('\n');
 			System.out.println(summary);
@@ -129,8 +145,10 @@ public class DynamicLexiconTrainer {
 	}
 
 	private AbstractClassifier openClassifier(int n) throws Exception {
-		final Path modelFile = environment.fullPath(environment.getDynamicLexiconModel(n));
-		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(modelFile.toFile()))) {
+		final Path modelFile = environment
+				.fullPath(environment.getDynamicLexiconModel(n));
+		try (ObjectInputStream in = new ObjectInputStream(
+				new FileInputStream(modelFile.toFile()))) {
 			return (SimpleLogistic) in.readObject();
 		}
 	}
