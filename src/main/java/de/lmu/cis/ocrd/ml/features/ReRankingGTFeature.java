921d9b8ce0e993d841bc1512571a518ebc7d7267
flo <flo@cis.lmu.de>
2018-12-14 15:18:44 +0100

    Add ground-truth feature for re-ranking

---
+++ src/main/java/de/lmu/cis/ocrd/ml/features/ReRankingGTFeature.java
@@ -0,0 +1,26 @@
+package de.lmu.cis.ocrd.ml.features;
+
+import de.lmu.cis.ocrd.profile.Candidate;
+
+import java.util.Optional;
+
+public class ReRankingGTFeature extends NamedBooleanFeature {
+	public ReRankingGTFeature() {
+		super("ReRankingGT");
+	}
+
+	@Override
+	public boolean handlesOCR(int i, int n) {
+		return handlesOnlyMasterOCR(i, n);
+	}
+
+	@Override
+	boolean doCalculate(OCRToken token, int i, int n) {
+		Optional<Candidate> cand = token.getProfilerCandidate();
+		if (!cand.isPresent()) {
+			return token.getGT().get().equals(token.getMasterOCR().toString());
+		}
+		return token.getGT().get().equals(cand.get().Suggestion);
+	}
+
+}
