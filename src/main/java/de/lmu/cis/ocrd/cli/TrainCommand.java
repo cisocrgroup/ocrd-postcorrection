86dfd0dd56ef4a12258b4dee249eac039e376e0b
flo <flo@cis.lmu.de>
2019-01-11 11:40:22 +0100

    Clean up

--- src/main/java/de/lmu/cis/ocrd/cli/TrainCommand.java
+++ src/main/java/de/lmu/cis/ocrd/cli/TrainCommand.java
@@ -80,8 +80,66 @@ public class TrainCommand extends AbstractMLCommand {
 			train(dleTrain, dleModel);
 			train(rrTrain, rrModel);
 		}
-
 		// dm must be trained after rr has been trained.
+		trainDM();
+	}
+
+	private void prepareDLEAndRR(List<OCRToken> tokens, int i) {
+		Logger.info("prepareDLEAndRR({})", i);
+		lm.setTokens(tokens);
+		prepareDLE(tokens, i);
+		prepareRR(tokens, i);
+	}
+
+	private void prepareDLE(List<OCRToken> tokens, int i) {
+		tokens.forEach((token)->{
+			final FeatureSet.Vector values =
+					dleFS.calculateFeatureVector(token, i+1);
+			dlew.writeFeatureVector(values);
+		});
+	}
+
+	private void prepareRR(List<OCRToken> tokens, int i) {
+		final int max = getParameter().maxCandidates;
+		tokens.forEach((token)->{
+			final List<Candidate> cs = token.getAllProfilerCandidates(max);
+			Logger.debug("adding {} candidates", cs.size());
+			cs.forEach((c)->{
+				OCRTokenWithCandidateImpl tc =
+						new OCRTokenWithCandidateImpl(token, c);
+				// Logger.debug("prepareRR: adding {} (Candidate: {}, GT: {})",
+				// 		tc.getMasterOCR().toString(),
+				// 		c.Suggestion,
+				// 		tc.getGT().toString());
+				final FeatureSet.Vector values =
+						rrFS.calculateFeatureVector(tc, i+1);
+				// Logger.debug(values);
+				rrw.writeFeatureVector(values);
+			});
+		});
+	}
+
+	private void prepareDM(List<OCRToken> tokens, int i) {
+		final int max = getParameter().maxCandidates;
+		tokens.forEach((token)->{
+			final List<Candidate> cs = token.getAllProfilerCandidates(max);
+			Logger.debug("adding {} candidates", cs.size());
+			cs.forEach((c)->{
+				OCRTokenWithCandidateImpl tc =
+						new OCRTokenWithCandidateImpl(token, c);
+				// Logger.debug("prepareDM: adding {} (Candidate: {}, GT: {})",
+				// 		tc.getMasterOCR().toString(),
+				// 		c.Suggestion,
+				// 		tc.getGT().toString());
+				final FeatureSet.Vector values =
+						dmFS.calculateFeatureVector(tc, i+1);
+				// Logger.debug(values);
+				dmw.writeFeatureVector(values);
+			});
+		});
+	}
+
+	private void trainDM() throws Exception {
 		for (int i = 0; i < getParameter().nOCR; i++) {
 			final Path dmTrain = tagPath(getParameter().dmTraining.training, i+1);
 			final Path dmModel = tagPath(getParameter().dmTraining.model, i+1);
@@ -109,80 +167,12 @@ public class TrainCommand extends AbstractMLCommand {
 		}
 	}
 
-	private void prepareDLEAndRR(List<OCRToken> tokens, int i) throws Exception {
-		Logger.info("prepareDLEAndRR({})", i);
-		lm.setTokens(tokens);
-		prepareDLE(tokens, i);
-		prepareRR(tokens, i);
-	}
-
-	private void prepareDLE(List<OCRToken> tokens, int i) throws Exception {
-		tokens.forEach((token)->{
-			final FeatureSet.Vector values =
-					dleFS.calculateFeatureVector(token, i+1);
-			dlew.writeFeatureVector(values);
-		});
-	}
-
-	private void prepareRR(List<OCRToken> tokens, int i) throws Exception {
-		final int max = getParameter().maxCandidates;
-		tokens.forEach((token)->{
-			final List<Candidate> cs = token.getAllProfilerCandidates(max);
-			Logger.debug("adding {} candidates", cs.size());
-			cs.forEach((c)->{
-				OCRTokenWithCandidateImpl tc =
-						new OCRTokenWithCandidateImpl(token, c);
-				// Logger.debug("prepareRR: adding {} (Candidate: {}, GT: {})",
-				// 		tc.getMasterOCR().toString(),
-				// 		c.Suggestion,
-				// 		tc.getGT().toString());
-				final FeatureSet.Vector values =
-						rrFS.calculateFeatureVector(tc, i+1);
-				// Logger.debug(values);
-				rrw.writeFeatureVector(values);
-			});
-		});
-	}
-
-	private void prepareDM(List<OCRToken> tokens, int i) throws Exception {
-		final int max = getParameter().maxCandidates;
-		tokens.forEach((token)->{
-			final List<Candidate> cs = token.getAllProfilerCandidates(max);
-			Logger.debug("adding {} candidates", cs.size());
-			cs.forEach((c)->{
-				OCRTokenWithCandidateImpl tc =
-						new OCRTokenWithCandidateImpl(token, c);
-				// Logger.debug("prepareDM: adding {} (Candidate: {}, GT: {})",
-				// 		tc.getMasterOCR().toString(),
-				// 		c.Suggestion,
-				// 		tc.getGT().toString());
-				final FeatureSet.Vector values =
-						dmFS.calculateFeatureVector(tc, i+1);
-				// Logger.debug(values);
-				dmw.writeFeatureVector(values);
-			});
-		});
-	}
-
 	private static void train(Path src, Path dest) throws Exception {
 		Logger.debug("training {} from {}", dest.toString(), src.toString());
 		LogisticClassifier classifier = LogisticClassifier.train(src);
 		classifier.save(dest);
-		// final ConverterUtils.DataSource ds = new ConverterUtils.DataSource(
-		// 		src.toString());
-		// final Instances train = ds.getDataSet();
-		// // gt is last class
-		// train.setClassIndex(train.numAttributes() - 1);
-		// final Instances structure = ds.getStructure();
-		// structure.setClassIndex(structure.numAttributes() - 1);
-		// final AbstractClassifier sl = new SimpleLogistic();
-		// sl.buildClassifier(train);
-		// try (ObjectOutputStream out = new ObjectOutputStream(
-		// 		new FileOutputStream(dest.toFile()))) {
-		// 	out.writeObject(sl);
-		// 	out.flush();
-		// }
 	}
+
 
 	private static Writer getWriter(Path path) throws Exception {
 		return new BufferedWriter(new FileWriter(path.toFile()));
