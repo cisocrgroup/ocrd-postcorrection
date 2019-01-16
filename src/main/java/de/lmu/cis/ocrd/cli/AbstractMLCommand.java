4c6eda21afb57786ce0752c837e49d9245d434c9
flo <flo@cis.lmu.de>
2019-01-16 10:04:39 +0100

    Disable logging

--- src/main/java/de/lmu/cis/ocrd/cli/AbstractMLCommand.java
+++ src/main/java/de/lmu/cis/ocrd/cli/AbstractMLCommand.java
@@ -6,7 +6,6 @@ import de.lmu.cis.ocrd.ml.features.*;
 import de.lmu.cis.ocrd.pagexml.*;
 import de.lmu.cis.ocrd.profile.Candidate;
 import org.apache.commons.io.IOUtils;
-import org.pmw.tinylog.Logger;
 import weka.core.Instance;
 
 import java.io.FileInputStream;
@@ -93,8 +92,8 @@ public abstract class AbstractMLCommand extends AbstractIOCommand {
 			final double confidence = prediction.getPrediction() ?
 					prediction.getConfidence() :
 					-prediction.getConfidence();
-			Logger.debug("Prediction for dm training: {}", prediction);
-			Logger.debug("confidence: {}", confidence);
+			// Logger.debug("Prediction for dm training: {}", prediction);
+			// Logger.debug("confidence: {}", confidence);
 			confidences.set(j, confidence);
 			j++;
 		}
