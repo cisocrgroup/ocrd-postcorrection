817a8bdef2298e5a34c68669f8b51a0bed0b7ac6
flo <flo@cis.lmu.de>
2019-03-05 13:14:12 +0100

    Implement zipped model files

---
+++ src/main/java/de/lmu/cis/ocrd/cli/ModelZIP.java
@@ -0,0 +1,149 @@
+package de.lmu.cis.ocrd.cli;
+
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipFile;
+
+public class ModelZIP implements Closeable {
+    private ZipFile zipFile;
+    private Config config;
+
+    public static ModelZIP open(Path path) throws Exception {
+        ModelZIP m = new ModelZIP();
+        m.zipFile = new ZipFile(path.toFile());
+        m.config = m.readConfig();
+        return m;
+    }
+
+    public InputStream openDLEModel(int i) throws Exception {
+        return openModel(config.dleModels, i);
+    }
+
+    public InputStream openRRModel(int i) throws Exception {
+        return openModel(config.rrModels, i);
+    }
+
+    public InputStream openDMModel(int i) throws Exception {
+        return openModel(config.dmModels, i);
+    }
+
+    public InputStream openDLEFeatureSet() throws Exception {
+        return zipFile.getInputStream(zipFile.getEntry(config.dleFeatureSet));
+    }
+
+    public InputStream openRRFeatureSet() throws Exception {
+        return zipFile.getInputStream(zipFile.getEntry(config.rrFeatureSet));
+    }
+
+    public ModelZIP addDLEModel(Path path, int i) {
+        initConfig();
+        addModel(config.dleModels, path, i);
+        return this;
+    }
+
+    public ModelZIP addRRModel(Path path, int i) {
+        initConfig();
+        addModel(config.rrModels, path, i);
+        return this;
+    }
+
+    public ModelZIP addDMModel(Path path, int i) {
+        initConfig();
+        addModel(config.dmModels, path, i);
+        return this;
+    }
+
+    public ModelZIP setDLEFeatureSet(Path path) {
+        initConfig();
+        config.dleFeatureSet = path.toString();
+        return this;
+    }
+
+    public ModelZIP setRRFeatureSet(Path path) {
+        initConfig();
+        config.rrFeatureSet = path.toString();
+        return this;
+    }
+
+    public void save(Path path) throws Exception {
+        Map<String, String> env = new HashMap<>();
+        env.put("create", "true");
+        URI uri = URI.create("jar:file:" + path.toString());
+        try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
+            for (int i = 0; i < config.dleModels.size(); i++) {
+                config.dleModels.set(i, copyInto(zipfs, config.dleModels.get(i)));
+            }
+            for (int i = 0; i < config.rrModels.size(); i++) {
+                config.rrModels.set(i, copyInto(zipfs, config.rrModels.get(i)));
+            }
+            for (int i = 0; i < config.dmModels.size(); i++) {
+                config.dmModels.set(i, copyInto(zipfs, config.dmModels.get(i)));
+            }
+            config.dleFeatureSet = copyInto(zipfs, config.dleFeatureSet);
+            config.rrFeatureSet = copyInto(zipfs, config.rrFeatureSet);
+            writeConfig(zipfs);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (zipFile != null) {
+            zipFile.close();
+        }
+    }
+
+    private void writeConfig(FileSystem fs) throws Exception {
+        final String json = new Gson().toJson(config);
+        Files.copy(new ByteArrayInputStream(json.getBytes(Charset.forName("UTF-8"))),
+                fs.getPath("config.json"), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private String copyInto(FileSystem fs, String path) throws Exception {
+        final Path p = Paths.get(path);
+        final String filename = p.getFileName().toString();
+        final Path zip = fs.getPath(filename);
+        Files.copy(p, zip, StandardCopyOption.REPLACE_EXISTING);
+        return filename;
+    }
+
+    private void addModel(List<String> models, Path path, int i) {
+        while (models.size() <= i) {
+            models.add("");
+        }
+        models.set(i, path.toString());
+    }
+
+    private void initConfig() {
+        if (config == null) {
+            config = new Config();
+            config.dleModels = new ArrayList<>();
+            config.rrModels = new ArrayList<>();
+            config.dmModels = new ArrayList<>();
+            config.dleFeatureSet = "";
+            config.rrFeatureSet = "";
+        }
+    }
+
+    private Config readConfig() throws Exception {
+        try (InputStream is = zipFile.getInputStream(zipFile.getEntry("config.json"))) {
+            return new Gson().fromJson(new InputStreamReader(is), Config.class);
+        }
+    }
+
+    private InputStream openModel(List<String> models, int i) throws Exception {
+        return zipFile.getInputStream(zipFile.getEntry(models.get(i)));
+    }
+
+    private static class Config {
+        List<String> dleModels, rrModels, dmModels;
+        String dleFeatureSet, rrFeatureSet;
+    }
+}
---
+++ src/test/java/de/lmu/cis/ocrd/cli/test/ModelZIPTest.java
@@ -0,0 +1,140 @@
+package de.lmu.cis.ocrd.cli.test;
+
+import de.lmu.cis.ocrd.cli.ModelZIP;
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ModelZIPTest {
+    private ModelZIP model;
+    private Path tmpdir;
+    private String[] names;
+
+    @Before
+    public void init() throws Exception {
+        tmpdir = Files.createTempDirectory("ocrd-cis-java");
+        model = new ModelZIP();
+        names = new String[]{
+                "a", "b", "c",
+                "d", "e", "f",
+                "g", "h", "i",
+                "j", "k",
+        };
+        Path out;
+        int j = 0;
+        for (int i = 0; i < 3; i++) {
+            out = writeTmpFile(names[j++]);
+            model.addDLEModel(out, i);
+        }
+        for (int i = 0; i < 3; i++) {
+            out = writeTmpFile(names[j++]);
+            model.addRRModel(out, i);
+        }
+        for (int i = 0; i < 3; i++) {
+            out = writeTmpFile(names[j++]);
+            model.addDMModel(out, i);
+        }
+        out = writeTmpFile(names[j++]);
+        model.setDLEFeatureSet(out);
+        out = writeTmpFile(names[j]);
+        model.setRRFeatureSet(out);
+        model.save(Paths.get(tmpdir.toString(), "model.zip"));
+        model = ModelZIP.open(Paths.get(tmpdir.toString(), "model.zip"));
+    }
+
+    @After
+    public void close() throws IOException {
+        model.close();
+        // tmpdir.toFile().delete();
+    }
+
+    private Path writeTmpFile(String x) throws IOException {
+        final Path out = Paths.get(tmpdir.toString(), x);
+        try (PrintWriter os = new PrintWriter(out.toString())) {
+            os.print(x);
+        }
+        return out;
+    }
+
+    private String readStringAndClose(InputStream is) throws IOException {
+        try (InputStream iis = is) {
+            return IOUtils.toString(iis, Charset.forName("UTF-8"));
+        }
+    }
+
+    @Test
+    public void testReadDLEModel1() throws Exception {
+        final String got = readStringAndClose(model.openDLEModel(0));
+        assertThat(got, is(names[0]));
+    }
+
+    @Test
+    public void testReadDLEModel2() throws Exception {
+        final String got = readStringAndClose(model.openDLEModel(1));
+        assertThat(got, is(names[1]));
+    }
+
+    @Test
+    public void testReadDLEModel3() throws Exception {
+        final String got = readStringAndClose(model.openDLEModel(2));
+        assertThat(got, is(names[2]));
+    }
+
+    @Test
+    public void testReadRRModel1() throws Exception {
+        final String got = readStringAndClose(model.openRRModel(0));
+        assertThat(got, is(names[3]));
+    }
+
+    @Test
+    public void testReadRRModel2() throws Exception {
+        final String got = readStringAndClose(model.openRRModel(1));
+        assertThat(got, is(names[4]));
+    }
+
+    @Test
+    public void testReadRRModel3() throws Exception {
+        final String got = readStringAndClose(model.openRRModel(2));
+        assertThat(got, is(names[5]));
+    }
+
+    @Test
+    public void testReadDMModel1() throws Exception {
+        final String got = readStringAndClose(model.openDMModel(0));
+        assertThat(got, is(names[6]));
+    }
+
+    @Test
+    public void testReadDMModel2() throws Exception {
+        final String got = readStringAndClose(model.openDMModel(1));
+        assertThat(got, is(names[7]));
+    }
+
+    @Test
+    public void testReadDMModel3() throws Exception {
+        final String got = readStringAndClose(model.openDMModel(2));
+        assertThat(got, is(names[8]));
+    }
+
+    @Test
+    public void testReadDLEFeatureSet() throws Exception {
+        final String got = readStringAndClose(model.openDLEFeatureSet());
+        assertThat(got, is(names[9]));
+    }
+
+    @Test
+    public void testReadRRFeatureSet() throws Exception {
+        final String got = readStringAndClose(model.openRRFeatureSet());
+        assertThat(got, is(names[10]));
+    }
+}
