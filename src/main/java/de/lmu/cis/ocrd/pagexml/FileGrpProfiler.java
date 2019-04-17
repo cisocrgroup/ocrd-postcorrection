08ff7d03fe627e56343c50b0701f49e3ad72ed14
flo <flo@cis.lmu.de>
2019-04-17 16:32:11 +0200

    Log profiler input

--- src/main/java/de/lmu/cis/ocrd/pagexml/FileGrpProfiler.java
+++ src/main/java/de/lmu/cis/ocrd/pagexml/FileGrpProfiler.java
@@ -2,6 +2,8 @@ package de.lmu.cis.ocrd.pagexml;
 
 import de.lmu.cis.ocrd.profile.AbstractProfiler;
 import de.lmu.cis.ocrd.profile.ProfilerProcess;
+import org.pmw.tinylog.Logger;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -23,6 +25,7 @@ public class FileGrpProfiler extends AbstractProfiler {
                 for (Word word: line.getWords()) {
                     List<String> unicode = word.getUnicodeNormalized();
                     if (!unicode.isEmpty() && !unicode.get(0).isEmpty()) {
+                        Logger.debug("profiler input: {}", unicode.get(0));
                         // append master ocr (one token per line)
                         b.append(unicode.get(0));
                         b.append(":\n");
