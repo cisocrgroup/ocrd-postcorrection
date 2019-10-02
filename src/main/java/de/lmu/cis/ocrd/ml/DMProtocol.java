5a7181acbed54d8df8000375607973d1ba01bcd4
flo <flo@cis.lmu.de>
2019-10-02 09:32:44 +0200

    Change protocol format: output unique token ids for corrections

--- src/main/java/de/lmu/cis/ocrd/ml/DMProtocol.java
+++ src/main/java/de/lmu/cis/ocrd/ml/DMProtocol.java
@@ -3,45 +3,33 @@ package de.lmu.cis.ocrd.ml;
 import com.google.gson.Gson;
 import de.lmu.cis.ocrd.ml.features.OCRToken;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DMProtocol implements Protocol {
     private static class Protocol {
-        private final Map<String, Integer> always = new HashMap<>();
-        private final Map<String, Integer> sometimes = new HashMap<>();
-        private final Map<String, Integer> never = new HashMap<>();
+        private final Map<String, List<String>> considered = new HashMap<>();
+        private final Map<String, List<String>> notConsidered = new HashMap<>();
     }
-    private static class Count {
-        int considered = 0;
-        int notConsidered = 0;
-    }
-    private final HashMap<String, Count> counts = new HashMap<>();
+
+    private final Protocol protocol = new Protocol();
 
     @Override
     public String toJSON() {
-        final Protocol protocol = new Protocol();
-        counts.forEach((word, count) -> {
-            if (count.considered > 0 && count.notConsidered > 0) {
-                protocol.sometimes.put(word, count.considered + count.notConsidered);
-            } else if (count.considered > 0) {
-                protocol.always.put(word, count.considered);
-            } else {
-                protocol.never.put(word, count.notConsidered);
-            }
-        });
         return new Gson().toJson(protocol);
     }
 
     @Override
     public void register(OCRToken token, boolean considered) {
         final String word = token.getMasterOCR().getWord().toLowerCase();
-        final Count count = counts.getOrDefault(word, new Count());
+        List<String> list;
         if (considered) {
-            count.considered += 1;
+            list = protocol.considered.computeIfAbsent(word, k -> new ArrayList<>());
         } else {
-            count.notConsidered += 1;
+            list = protocol.notConsidered.computeIfAbsent(word, k -> new ArrayList<>());
         }
-        counts.put(word, count);
+        list.add(token.getMasterOCR().id());
     }
 }
