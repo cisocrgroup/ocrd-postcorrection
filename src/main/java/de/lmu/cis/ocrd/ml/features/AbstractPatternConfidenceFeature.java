e86fa411d4f59170d3ba71edcd954f0644827c0b
flo <flo@cis.lmu.de>
2019-11-19 15:25:57 +0100

    Implement feature for ocr confidence at a pattern position

---
+++ src/main/java/de/lmu/cis/ocrd/ml/features/AbstractPatternConfidenceFeature.java
@@ -0,0 +1,53 @@
+package de.lmu.cis.ocrd.ml.features;
+
+import com.google.gson.JsonObject;
+import de.lmu.cis.ocrd.profile.PosPattern;
+import de.lmu.cis.ocrd.util.JSON;
+
+public abstract class AbstractPatternConfidenceFeature extends NamedDoubleFeature {
+    public AbstractPatternConfidenceFeature(JsonObject o, ArgumentFactory args) {
+        this(JSON.mustGetNameOrType(o));
+    }
+
+    AbstractPatternConfidenceFeature(String name) {
+        super(name);
+    }
+
+    // Calculate the OCR confidence for the given pattern.
+    // OCR:       seniper
+    // Candidate: semper
+    // Pattern:   m:ni:2
+    // c(i):      get confidence for OCR at pos i
+    // Result:    c(2) * c(3)
+    double calculateConfidence(OCRWord word, PosPattern pattern) {
+        if (pattern.Left.isEmpty()) {
+            return calculateConfidenceForInsertion(word, pattern);
+        }
+        return calculateConfidenceForSubstitution(word, pattern);
+    }
+
+    // Use the product of the substituted characters.
+    private static double calculateConfidenceForSubstitution(OCRWord word, PosPattern pattern) {
+        double ret = 1;
+        final int[] cp = pattern.Left.codePoints().toArray();
+        for (int i = 0; i < cp.length; i++) {
+            ret *= word.getCharacterConfidenceAt(pattern.Pos + i);
+        }
+        return ret;
+    }
+
+    // There are three possible positions:
+    // at 0: use confidence for character at pos=0
+    // at len: use confidence for last character at pos=len-1
+    // else: use confidence for surrounding characters (pos+pos-1)/2
+    private static double calculateConfidenceForInsertion(OCRWord word, PosPattern pattern) {
+        if (pattern.Pos == 0) {
+            return word.getCharacterConfidenceAt(0);
+        }
+        final int[] cp = word.getWordNormalized().codePoints().toArray();
+        if (pattern.Pos > cp.length) {
+            return word.getCharacterConfidenceAt(cp.length-1);
+        }
+        return (word.getCharacterConfidenceAt(pattern.Pos) + word.getCharacterConfidenceAt(pattern.Pos - 1)) / 2.0;
+    }
+}
---
+++ src/main/java/de/lmu/cis/ocrd/ml/features/CandidateOCRPatternConfidenceFeature.java
@@ -0,0 +1,31 @@
+package de.lmu.cis.ocrd.ml.features;
+
+import com.google.gson.JsonObject;
+import de.lmu.cis.ocrd.profile.Candidate;
+import de.lmu.cis.ocrd.util.JSON;
+
+public class CandidateOCRPatternConfidenceFeature extends AbstractPatternConfidenceFeature {
+    public CandidateOCRPatternConfidenceFeature(JsonObject o, ArgumentFactory args) {
+        this(JSON.mustGetNameOrType(o));
+    }
+
+    private CandidateOCRPatternConfidenceFeature(String name) {
+        super(name);
+    }
+
+    @Override
+    protected double doCalculate(OCRToken token, int i, int n) {
+        final Candidate candidate = mustGetCandidate(token);
+        final OCRWord word = getWord(token, i, n);
+        double ret = 1; // if the token does not have any ocr-patterns we just return 1.
+        for (int j = 0; j < candidate.OCRPatterns.length; j++) {
+            ret *= calculateConfidence(word, candidate.OCRPatterns[j]);
+        }
+        return ret;
+    }
+
+    @Override
+    public boolean handlesOCR(int i, int n) {
+        return handlesOnlyMasterOCR(i, n);
+    }
+}
