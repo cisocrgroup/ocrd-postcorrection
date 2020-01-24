225fd0e7ff7d137fa6e3da76b8039a8379400223
flo <flo@cis.lmu.de>
2020-01-24 17:03:29 +0100

    Clean up

--- src/test/java/de/lmu/cis/ocrd/align/test/AlignmentGraphIteratorTest.java
+++ src/test/java/de/lmu/cis/ocrd/align/test/AlignmentGraphIteratorTest.java
@@ -21,7 +21,7 @@ public class AlignmentGraphIteratorTest {
 		String a = "diee Presse";
 		String b = "die Preſſe";
 		Graph g = new Graph(a, b);
-		System.out.println(g.getStartNode());
+		// System.out.println(g.getStartNode());
 		assertThat(makeString(g, 0), is('#' + a + '$'));
 		assertThat(makeString(g, 1), is('#' + b + '$'));
 	}
@@ -31,7 +31,7 @@ public class AlignmentGraphIteratorTest {
 		String a = "die Presse";
 		String b = "di Preſſe";
 		Graph g = new Graph(a, b);
-		System.out.println(g.getStartNode());
+		// System.out.println(g.getStartNode());
 		assertThat(makeString(g, 0), is('#' + a + '$'));
 		assertThat(makeString(g, 1), is('#' + b + '$'));
 	}
