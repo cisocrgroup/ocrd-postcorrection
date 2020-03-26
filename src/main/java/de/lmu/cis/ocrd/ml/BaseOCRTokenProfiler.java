cce2d7ae34e2786ef9c7b70e5ca47032a6910181
flo <flo@cis.lmu.de>
2020-03-26 13:18:07 +0100

    Handle empty profiles

--- src/main/java/de/lmu/cis/ocrd/ml/BaseOCRTokenProfiler.java
+++ src/main/java/de/lmu/cis/ocrd/ml/BaseOCRTokenProfiler.java
@@ -18,9 +18,14 @@ public class BaseOCRTokenProfiler extends AbstractProfiler {
     @Override
     protected InputStream open() throws Exception {
         StringBuilder b = new StringBuilder();
+        boolean haveAtLeastOneToken = false;
         for (BaseOCRToken token: baseOCRTokenReader.read()) {
             b.append(token.getMasterOCR().getWordNormalized());
             b.append(":\n");
+            haveAtLeastOneToken = true;
+        }
+        if (!haveAtLeastOneToken) {
+            return null;
         }
         return new ByteArrayInputStream(b.toString().getBytes(StandardCharsets.UTF_8));
     }
